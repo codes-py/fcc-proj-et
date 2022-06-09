902e78c1473bada00379c83d76603b298e816d67
codes-py <63456076+codes-py@users.noreply.github.com>
2022-06-09 15:18:56 +0530

    improved

--- index.js
+++ index.js
@@ -1,93 +1,101 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-const conn = mongoose.connect(process.env['DB_URI'])
-  .then(() => console.log('connected to db'))
+mongoose.connect(process.env.MONGO_DB)
+  .then(() => console.log('db connected'))
   .catch(console.log)
 
 const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true
-  },
-  log: [{
-    type: {
-      description: {
-        type: String,
-        required: true
-      },
-      duration: {
-        type: Number,
-        required: true,
-      },
-      date: {
-        type: String,
-        default: new Date().toDateString()
-      }
-    },
-    default: []
-  }]
+  }
 })
 
+const exerciseSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
 
 const User = mongoose.model('User', userSchema)
-const error = { error: 'An errorrr' }
+const Exercise = mongoose.model('Exercise', exerciseSchema)
+const handleError = (err, res) => res.json({error: err.message})
 
-app.post('/api/users', (req, res) => {
-  const { username } = req.body;
-  new User({ username }).save((err, data) => {
-    if (err) return res.json(error);
-    return res.json({ username: data.username, _id: data._id });
-  })
+app.post('/api/users/', (req, res) => {
+  new User(req.body).save()
+    .then(data => res.json(data))
+    .catch(err => handleError(err, res))
 })
 
 app.get('/api/users', (req, res) => {
-  User.find({}).select({ username: 1, _id: 1 }).exec((err, data) => {
-    if (err) return res.json(error)
-    return res.json(data)
-  })
+  User.find().select('username _id').exec()
+    .then(data => res.json(data))
+    .catch(err => handleError(err, res))
 })
 
 app.post('/api/users/:_id/exercises', (req, res) => {
-  const exercise = {
-    description: req.body.description,
-    duration: parseInt(req.body.duration),
-    date: req.body.date || new Date().toDateString()
-  }
-  console.log(exercise)
-  User.findByIdAndUpdate(req.params._id, { $push: { log: exercise } }, (err, data) => {
-    if (err) return res.json(error)
-    return res.json({
-      _id: data._id,
-      username: data.username,
-      ...exercise
+  const { description, duration, date } = req.body
+  User.findById(req.params._id).select('username _id').exec()
+    .then(user => {
+      new Exercise({ userId: user._id, description, duration, date: date || new Date() }).save()
+        .then(ex => res.json({ ...user._doc, description, duration: ex.duration, date: ex.date.toDateString() }))
+        .catch(err => handleError(err, res))
     })
-  })
+    .catch(err => handleError(err, res))
 })
 
 app.get('/api/users/:_id/logs', (req, res) => {
-  const { _id } = req.params;
-  User.findById(_id).select({ _id: 1, username: 1, log: 1 }).exec((err, data) => {
-    if (err) return res.json(error)
-    return res.json({
-      ...data._doc,
-      count: data.log.length
+	console.log(req.query);
+  const { _id: id } = req.params
+	let from, to, limit = parseInt(req.query.limit);
+	if (req.query.from)
+		from = new Date(req.query.from)
+	else from = new Date(0)
+	if (req.query.to)
+		to = new Date(req.query.to)
+	else to = new Date()
+	if (isNaN(limit)) limit = 10000;
+	
+  User.findById(id).select('username _id').exec()
+    .then(user => {
+      Exercise.find({ userId: id }).where('date').gte(from).lte(to)
+        .limit(limit).select('description duration date -_id').exec()
+        .then(exs => {
+                    const exd = exs.map(ex => ({ ...ex._doc, date: ex.date.toDateString() }))
+		console.log({ ...user._doc, count: exs.length, log: exd })
+			return res.json({ ...user._doc, count: exs.length, log: exd })
+		})
+        .catch(err => handleError(err, res))
     })
-  })
+    .catch(err => handleError(err, res))
 })
 
 
-const listener = app.listen(process.env.PORT || 3001, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
